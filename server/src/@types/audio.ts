eaf87f7fd2d49a8a1422f481106b4d0c4a66fed3
Huyboken <huyboken@gmail.com>
2023-10-27 22:34:33 +0700

    playlist api

--- server/src/@types/audio.ts
+++ server/src/@types/audio.ts
@@ -1,4 +1,22 @@
 import { AudioDocument } from "#/models/audio";
+import { Request } from "express";
 import { ObjectId } from "mongoose";
 
 export type PopulateFavList = AudioDocument<{ _id: ObjectId; name: string }>;
+
+export interface CreatePlaylistRequest extends Request {
+  body: {
+    title: string;
+    resId: string;
+    visibility: "public" | "private";
+  };
+}
+
+export interface UpdatePlaylistRequest extends Request {
+  body: {
+    title: string;
+    item: string;
+    id: string;
+    visibility: "public" | "private";
+  };
+}
--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -69,3 +69,29 @@ export const AudioValidationSchema = yup.object().shape({
     .oneOf(categories, "Invalid category!")
     .required("Category is missing!"),
 });
+
+export const NewPlaylistValidationSchema = yup.object().shape({
+  title: yup.string().required("Title is missing!"),
+  resId: yup.string().transform(function (value) {
+    return this.isType(value) && isValidObjectId(value) ? value : "";
+  }),
+  visibility: yup
+    .string()
+    .oneOf(["public", "private"], "Visibility must be public or private!")
+    .required("Visibility is missing!"),
+});
+export const OldPlaylistValidationSchema = yup.object().shape({
+  title: yup.string().required("Title is missing!"),
+  //This is going to validate the audio id
+  item: yup.string().transform(function (value) {
+    return this.isType(value) && isValidObjectId(value) ? value : "";
+  }),
+  //This is going to validate the playlist id
+  id: yup.string().transform(function (value) {
+    return this.isType(value) && isValidObjectId(value) ? value : "";
+  }),
+  visibility: yup
+    .string()
+    .oneOf(["public", "private"], "Visibility must be public or private!"),
+  // .required("Visibility is missing!"),
+});
